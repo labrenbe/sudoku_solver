8304b9bae3070edc3772fc254b383e19f9540dd1
Bene <benedikt@labrenz.org>
2020-07-05 16:13:09 +0200

    created basic data model and index view

--- application/models.py
+++ application/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class SudokuState(models.Model):
+    id = models.IntegerField(primary_key=True)
+    timestamp = models.DateTimeField
+    state = models.CharField(max_length=200)
+    shape = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.state

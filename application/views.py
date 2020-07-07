ea7cfa7d65aabf22e8cbdc9fbb8b21fc60454f69
Bene <benedikt@labrenz.org>
2020-07-07 12:58:59 +0200

    attempts to implement request

--- application/views.py
+++ application/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 from django.template import loader
 
-
-def index(request):
+def solve(request):
     context = {}
-    return render(request, 'application/index.html', context)
+    for y in range(9):
+        for x in range(9):
+            cell = str(x) + str(y)
+            context[cell] = request.POST.get(cell)
+    return render(request, 'application/sudoku_solver.html', context)

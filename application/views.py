ac379996fb76888e18fd187caf4707216695ca76
Bene <benedikt@labrenz.org>
2020-07-15 12:05:49 +0200

    integrated frontend and backend

--- application/views.py
+++ application/views.py
@@ -5,12 +5,24 @@ from django.http import HttpResponse
 # Create your views here.
 from django.template import loader
 
+from solver_logic.solve_sudoku import solve_sudoku
+
 
 def solve(request):
     matrix = np.zeros((9, 9), dtype=int)
-    context = {'matrix': matrix}
     for y in range(9):
         for x in range(9):
             value = request.POST.get(str(y) + ':' + str(x))
             matrix[y][x] = value if value != '' else 0
+    blocks = np.array([[1, 1, 1, 2, 2, 2, 3, 3, 3],
+                      [1, 1, 1, 2, 2, 2, 3, 3, 3],
+                      [1, 1, 1, 2, 2, 2, 3, 3, 3],
+                      [4, 4, 4, 5, 5, 5, 6, 6, 6],
+                      [4, 4, 4, 5, 5, 5, 6, 6, 6],
+                      [4, 4, 4, 5, 5, 5, 6, 6, 6],
+                      [7, 7, 7, 8, 8, 8, 9, 9, 9],
+                      [7, 7, 7, 8, 8, 8, 9, 9, 9],
+                      [7, 7, 7, 8, 8, 8, 9, 9, 9]])
+    matrix = solve_sudoku(matrix, blocks)
+    context = {'matrix': matrix}
     return render(request, 'application/sudoku_solver.html', context)
